1c7ae4f03a60f2beab72e3cf1ff8bd815cc43b70
hartorn <bazirehoussin@gmail.com>
2013-07-09 17:15:45 +0200

    ajout du formulaire d ajout d un intern

--- univnews/interns.php
+++ univnews/interns.php
@@ -41,7 +41,7 @@
     
     	<div class="container">
     		
-				<button class="btn btn-small btn btn-info" type="button"><i class="icon-plus"></i> Add an Intern</button>
+			<button class="btn btn-small btn btn-info" type="button" data-toggle='modal' data-target='#addIntern'><i class="icon-plus"></i> Add an Intern</button>
 			    
 	    	<table class="table table-hover table-striped">
 	    		<thead>
@@ -96,6 +96,69 @@
 			<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
 		</div>
 	</div>
+	
+	
+	<div id="addIntern" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+			<h3 id="addInternLabel">Add an Intern</h3>
+		</div>
+			<div class="modal-body">
+			<form class="form-horizontal">
+			<div class="control-group">
+			<label class="control-label" for="inputLogin">Login</label>
+				<div class="controls">
+				<input type="text" id="Login" placeholder="Login">
+				</div>
+			</div>
+			
+			<div class="control-group">
+			<label class="control-label" for="inputName">First name</label>
+				<div class="controls">
+				<input type="text" id="firstName" placeholder="First name">
+				</div>
+			</div>
+			
+			<div class="control-group">
+			<label class="control-label" for="inputName">Last name</label>
+				<div class="controls">
+				<input type="text" id="Name" placeholder="Last name">
+				</div>
+			</div>
+			
+			<div class="control-group">
+			<label class="control-label" for="inputEmail">Email</label>
+				<div class="controls">
+				<input type="text" id="inputEmail" placeholder="Email">
+				</div>
+			</div>
+			
+			<div class="control-group">
+			<label class="control-label" for="inputPhone">Phone</label>
+				<div class="controls">
+				<input type="text" id="inputPhone" placeholder="Phone">
+				</div>
+			</div>
+			
+			<div class="control-group">
+			<label class="control-label" for="inputCountry">Country</label>
+				<div class="controls">
+				<input type="text" id="inputCountry" placeholder="Country" data-provide="typeahead" data-items="4" data-source='["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]' autocomplete="off">
+				</div>
+			</div>
+						
+			<div class="control-group">
+				<div class="controls">
+				<button type="submit" class="btn">Submit</button>
+				</div>
+			</div>
+			</form>
+		</div>
+		<div class="modal-footer">
+			<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+		</div>
+	</div>	
+	
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="./bootstrap/js/bootstrap.min.js"></script>
 </body>
