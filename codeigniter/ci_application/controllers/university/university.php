9d1e7c483b35fd081cf3cfd0967408f60d00e7c7
Hartorn <bazirehoussin@gmail.com>
2013-07-18 17:54:36 +0200

    avant getter

--- codeigniter/ci_application/controllers/university/university.php
+++ codeigniter/ci_application/controllers/university/university.php
@@ -2,10 +2,18 @@
  
 class University extends CI_Controller
 {
-	function University()
-    {
-        parent::__construct(); 
-    }
+	public $id;
+	
+	public function __construct($id = false){
+		parent::__construct(); 
+		if(isset($id)){
+			$this->load->model('university_md');
+					
+			$this->id = $id;
+			$this->initialiseValue();	
+		}
+		
+	}
 
     public function index()
     {
@@ -25,7 +33,27 @@ class University extends CI_Controller
     
      public function get()
      {
-		 
+		$ci = new CI_CONTROLLER();
+		$ci->load->model('university_md');
+		$id = $ci->uri->segment(4);
+		
+		$university = new University($id);
+		
+		exit (json_encode(
+							array(
+									"id" => $university->get_id(), 
+									"name" => $university->get_name(), 
+									"address" => $university->get_adress(),
+									"country" => $university->get_country(), 
+									"subscription" => $university->get_subscription(), 
+									"checking state" => $university->get_checking_state(),  
+									"comment" => $university->get_comment()
+							)
+		));
 	 }
 
+     public function get()
+     {
+		 
+	 }
 }
