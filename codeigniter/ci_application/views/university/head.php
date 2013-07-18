05d26b1c45f7f37510f1a33fd8a2e47491c88d6e
Studios92.com Ltd <studios92@IT120.(none)>
2013-07-18 09:25:58 +0100

    Controller

--- codeigniter/ci_application/views/university/head.php
+++ codeigniter/ci_application/views/university/head.php
@@ -3,9 +3,12 @@
 <head>
 	<title>University Newsletter Management</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Bootstrap -->
-	<?php foreach($css as $url): ?>
+	<!-- Bootstrap 
+	<?php foreach($c as $url): ?>
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $url; ?>" />
-	<?php endforeach; ?>
+	<?php endforeach; ?>-->
+	
 </head>
 <body>
+
+<!--<?php echo $css; ?>-->
