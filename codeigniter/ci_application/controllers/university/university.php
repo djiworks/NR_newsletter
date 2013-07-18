d38d3badb8b06b03bd889edceb42d85254038d2b
Hartorn <bazirehoussin@gmail.com>
2013-07-18 17:27:11 +0200

    suppression de university.php hors du dossier university

--- codeigniter/ci_application/controllers/university/university.php
+++ codeigniter/ci_application/controllers/university/university.php
@@ -9,18 +9,18 @@ class University extends CI_Controller
 
     public function index()
     {
-		$this->load->view('university/body');
+		//~ echo 'coucou';
+		$this->load->view('university/head');
 		$this->load->view('university/topmenu');
 		$this->load->view('university/leftmenu');
+		$this->load->view('university/body');
 		$this->load->view('university/footer');
 
     }
     
     public function accueil()
     {
-		//$this->load->view('university/body');
-		$this->load->view('university/topmenu');
-		$this->load->view('university/leftmenu');
-		//$this->load->view('university/footer');
+		$this->index();
     }
+    
 }
