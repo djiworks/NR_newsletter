dce4ec5d4a167c90bc14ed957cfef83c1f471587
Hartorn <bazirehoussin@gmail.com>
2013-07-18 11:25:08 +0200

    ajout des fonctionnalites de base dans le modele

--- codeigniter/ci_application/models/university_md.php
+++ codeigniter/ci_application/models/university_md.php
@@ -1,1 +1,46 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+class University_md extends CI_Model
+{
+	public $table = "university";
+	
+	public function update($id, $name, $adresse, $country, $subscription, $checking_state){
+		 
+		$this->db->set("name",$name)
+				 ->set("adresse",$adresse)
+				 ->set("country",$country)
+				 ->set("subscription",$subscription)
+				 ->set("checking_state",$checking_state)
+				 ->where("id",$id)
+				 ->update($this->table);
+	}
+	 
+	 public function get($id){
+		 return $this->db->where("id",$id)
+		 			 	 ->get($this->table);
+	 }
+	 
+	 public function create($name, $adresse, $country, $subscription, $checking_state){
+		$this->db->set("name",$name)
+				 ->set("adresse",$adresse)
+				 ->set("country",$country)
+				 ->set("subscription",$subscription)
+				 ->set("checking_state",$checking_state)
+		 		 ->insert($this->table);  
+	 }
+	 
+	 public function getSearchedUniversities($field, $value){
+		 return $this->db->query("
+					SELECT id
+					FROM ".$this->table."
+					WHERE ".$this->table.".".$field." LIKE '%".$value."%'
+			");
+	 }
+	 
+	public function delete($id_university){			 
+		$this->db->where('id_university', $id_university)
+				 ->delete($this->table);	
+	}
+	 
+	 
+}
