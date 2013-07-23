501a3307d1ccd7a2b35f951e0ba56446a193efcb
hartorn <bazirehoussin@gmail.com>
2013-07-23 12:38:04 +0200

    mise en place du squelette pour newsletter.php

---
+++ codeigniter/ci_application/models/newsletter/newsletter_md.php
@@ -0,0 +1,43 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Newsletter_md extends CI_Model
+{
+	private $table = "newsletter";
+	private $table2 = "contact";
+	
+	public function update(){
+	}
+	 
+	 public function addCommentOnNewsletter($id, $comment){
+		 return $this->db->query("
+		 			UPDATE ".$this->table."
+		 			SET comment = CONCAT(comment,  '".$comment."')
+					WHERE ".$this->table.".id_newsletter = " .$id
+					);
+	 }
+	 
+	 public function get($id){
+		return $this->db->where("id_newsletter",$id)
+						->get($this->table);
+	 }
+	 
+	 public function create(){
+ 
+	 }
+	 
+	 public function getSearchedNewsletter($field, $value){
+		 return $this->db->query("
+					SELECT id_newsletter
+					FROM ".$this->table."
+					WHERE ".$this->table.".".$field." LIKE '%".$value."%'
+			");
+	 }
+	 
+	public function delete($id_newsletter){			 
+
+	}
+	 
+	public function getAll() {
+
+	}
+}
---
+++ codeigniter/ci_application/views/newsletter/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>University Newsletter Management</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css" media="screen" href="/assets/bootstrap/css/bootstrap.min.css" /> 
+	<link rel="stylesheet" type="text/css" media="screen" href="/assets/css/university.css" /> 
+</head>
+<body>
