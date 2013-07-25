c1df467aaf9ec0ee5890f70173f000c200bdccc8
hartorn <bazirehoussin@gmail.com>
2013-07-25 10:06:32 +0200

    suppression d'un tr

--- codeigniter/ci_application/controllers/university/university.php
+++ codeigniter/ci_application/controllers/university/university.php
@@ -1,8 +1,6 @@
 <?php
-include_once(APPPATH."controllers/intern/intern.php");
- 
-class University extends CI_Controller
-{
+include_once (APPPATH . "controllers/intern/intern.php");
+class University extends CI_Controller {
 	private $id;
 	private $name;
 	private $address;
@@ -10,143 +8,134 @@ class University extends CI_Controller
 	private $subscription;
 	private $checking_state;
 	private $comment;
-	
-	public function __construct($id = false){
-		parent::__construct(); 
-		$this->load->model('university/university_md');
-		$this->load->database();
-
-		if($id){
-					
+	public function __construct($id = false) {
+		parent::__construct ();
+		$this->load->model ( 'university/university_md' );
+		$this->load->database ();
+		
+		if ($id) {
+			
 			$this->id = $id;
-			$this->initialiseValue();	
+			$this->initialiseValue ();
 		}
 	}
-
-    public function index()
-    {
-		$data = array();
-		$data['allUniv'] = $this->getAllUniversities();
-		$data['allNames'] = Intern::getAllNames();
+	public function index() {
+		$data = array ();
+		$data ['allUniv'] = $this->getAllUniversities ();
+		$data ['allNames'] = Intern::getAllNames ();
 		
-		$this->load->view('university/head');
-		$this->load->view('university/topmenu');
-		$this->load->view('university/leftmenu');
-		$this->load->view('university/body', $data);
-		$this->load->view('university/footer');
-    }
-    
-    public function accueil()
-    {
-		$this->index();
-    }
-    
-	public function getAllUniversities()
-	{
-		$ci = new CI_CONTROLLER();
-		$ci->load->model('university/university_md');
-		$this->load->database();
+		$this->load->view ( 'university/head' );
+		$this->load->view ( 'university/topmenu' );
+		$this->load->view ( 'university/leftmenu' );
+		$this->load->view ( 'university/body', $data );
+		$this->load->view ( 'university/footer' );
+	}
+	public function accueil() {
+		$this->index ();
+	}
+	public function getAllUniversities() {
+		$ci = new CI_CONTROLLER ();
+		$ci->load->model ( 'university/university_md' );
+		$this->load->database ();
 		
-		/***************************************************************
-		 * 				  Preparing the table of interns			   *
-		 ***************************************************************/ 
-		$fetched_intern = $ci->university_md->getAllUniv_Interns();
-		$displayInterns = array();
+		/**
+		 * *************************************************************
+		 * Preparing the table of interns *
+		 * *************************************************************
+		 */
+		$fetched_intern = $ci->university_md->getAllUniv_Interns ();
+		$displayInterns = array ();
 		$id_univ = "";
 		
-		foreach($fetched_intern->result() as $line) {
+		foreach ( $fetched_intern->result () as $line ) {
 			$id_univ = $line->id_university;
 			
-			if(!isset($displayInterns[$id_univ])) {
-				$displayInterns[$id_univ] = "";
+			if (! isset ( $displayInterns [$id_univ] )) {
+				$displayInterns [$id_univ] = "";
 			}
 			
-			switch($line->is_student) {
-				case 0:
+			switch ($line->is_student) {
+				case 0 :
 					$student = "No";
 					break;
-				case 1:
+				case 1 :
 					$student = "Yes";
 					break;
-				default:
+				default :
 					$student = "N/A";
 					break;
 			}
 			
-			$displayInterns[$id_univ] = $displayInterns[$id_univ].
-				"<tr>
-					<td>".$line->id_person."</td>
-					<td>".$student."</td>
-					<td>".$line->first_name." ".$line->last_name."</td>
-					<td>".$line->worked_until."</td>
+			$displayInterns [$id_univ] = $displayInterns [$id_univ] . "<tr>
+					<td>" . $line->id_person . "</td>
+					<td>" . $student . "</td>
+					<td>" . $line->first_name . " " . $line->last_name . "</td>
+					<td>" . $line->worked_until . "</td>
 				</tr>";
 		}
-
-/*
-echo '$displayInterns[1] = '.$displayInterns["1"].'<br />';
-echo '$displayInterns[2] = '.$displayInterns["2"].'<br />';
-echo '$displayInterns[3] = '.$displayInterns["3"].'<br />';
-echo '$displayInterns[4] = '.$displayInterns["4"].'<br />';
-echo '$displayInterns[5] = '.$displayInterns["5"].'<br />';
-echo '$displayInterns[6] = '.$displayInterns["6"].'<br />';
-*/
 		
-		/***************************************************************
-		 * 				  Preparing the table of contacts			   *
-		 ***************************************************************/
-		$fetched_Contact = $ci->university_md->getAllUniv_Contact();
-		$displayContact = array();
+		/*
+		 * echo '$displayInterns[1] = '.$displayInterns["1"].'<br />'; echo '$displayInterns[2] = '.$displayInterns["2"].'<br />'; echo '$displayInterns[3] = '.$displayInterns["3"].'<br />'; echo '$displayInterns[4] = '.$displayInterns["4"].'<br />'; echo '$displayInterns[5] = '.$displayInterns["5"].'<br />'; echo '$displayInterns[6] = '.$displayInterns["6"].'<br />';
+		 */
+		
+		/**
+		 * *************************************************************
+		 * Preparing the table of contacts *
+		 * *************************************************************
+		 */
+		$fetched_Contact = $ci->university_md->getAllUniv_Contact ();
+		$displayContact = array ();
 		$id_univ = "";
 		$i = 1;
 		
-		foreach($fetched_Contact->result() as $line) {
+		foreach ( $fetched_Contact->result () as $line ) {
 			$id_univ = $line->id_university;
 			
-			if(!isset($displayContact[$id_univ])) {
-				$displayContact[$id_univ] = "";
+			if (! isset ( $displayContact [$id_univ] )) {
+				$displayContact [$id_univ] = "";
 			}
 			
-			$displayContact[$id_univ] = $displayContact[$id_univ] . 
-				"<tr>
-					<td>".$line->id_contact."</td>
-					<td>".$line->information."</td>
-					<td>".$line->mail."</td>
-					<td>".$line->number."</td>
+			$displayContact [$id_univ] = $displayContact [$id_univ] . "<tr>
+					<td>" . $line->id_contact . "</td>
+					<td>" . $line->information . "</td>
+					<td>" . $line->mail . "</td>
+					<td>" . $line->number . "</td>
 				</tr>";
 		}
 		
-		
-		/***************************************************************
-		 * 				Preparing the table of universities  		   *
-		 ***************************************************************/
-		$fetched_univ = $ci->university_md->getAll();
+		/**
+		 * *************************************************************
+		 * Preparing the table of universities *
+		 * *************************************************************
+		 */
+		$fetched_univ = $ci->university_md->getAll ();
 		$result = "";
 		$id_univ = "";
 		$i = 1;
 		
-		foreach($fetched_univ->result() as $line) {
-			if($id_univ != $line->id_university) {
+		foreach ( $fetched_univ->result () as $line ) {
+			if ($id_univ != $line->id_university) {
 				$id_univ = $line->id_university;
 				
-				//Switching the state number to the real values
-				switch($line->checking_state) {
-					case 0:
+				// Switching the state number to the real values
+				switch ($line->checking_state) {
+					case 0 :
 						$classUniv = "";
 						$state = "First newsletter sent";
 						break;
-					case 1:
+					case 1 :
 						$classUniv = "success";
 						$state = "Approved";
 						break;
-					case 2:
+					case 2 :
 						$classUniv = "warning";
 						$state = "Waiting";
 						break;
-					case 3:
+					case 3 :
 						$classUniv = "error";
 						$state = "Wrong";
 						break;
-					default:
+					default :
 						$classUniv = "N/A";
 						$state = "N/A";
 						break;
@@ -154,21 +143,23 @@ echo '$displayInterns[6] = '.$displayInterns["6"].'<br />';
 				
 				$subcription = ($line->subscription == 1) ? "Yes" : "No";
 				
-				$result = $result."
+				$result = $result . "
 					<tr>
-						<tr class='".$classUniv."' id='princLine".$i."'>
+						<tr class='accordion-toggle " . $classUniv . "' data-toggle='collapse' data-parent='#accordion" . $i . "' href='#collapse" . $i . "' id='princLine" . $i . "'>
 							<td>
-								<input type='checkbox' id='chk".$i."' onclick='selectedUniv(\"".$line->name."\", \"".$line->id_university."\", \"chk".$i."\")'>
+								<input type='checkbox' id='chk" . $i . "' onclick='selectedUniv(\"" . $line->name . "\", \"" . $line->id_university . "\", \"chk" . $i . "\")'>
 							</td>
-							<td>".$line->id_university."</td>
-							<td>".$line->name."</td>
-							<td>".$line->address."</td>
-							<td>".$line->country."</td>
-							<td>".$subcription."</td>
-							<td>".$state."</td>
+							<td>" . $line->id_university . "</td>
+							<td>" . $line->name . "</td>
+							<td>" . $line->address . "</td>
+							<td>" . $line->country . "</td>
+							<td>" . $subcription . "</td>
+							<td>" . $state . "</td>
 							<td><a href='#viewdetail' data-toggle='modal'>Click here</a></td>
+						
 						</tr>
-						<tr class='".$classUniv."' id='secLine".$i."'>
+
+						<tr class='" . $classUniv . "' id='secLine" . $i . "'>
 							<td colspan='3'>
 								<table>
 									<thead>
@@ -180,7 +171,7 @@ echo '$displayInterns[6] = '.$displayInterns["6"].'<br />';
 										</tr>
 									</thead>
 									<tbody>
-										".$displayInterns[$line->id_university]."
+										" . $displayInterns [$line->id_university] . "
 									</tbody>
 								</table>
 							</td>
@@ -195,85 +186,71 @@ echo '$displayInterns[6] = '.$displayInterns["6"].'<br />';
 										</tr>
 									</thead>
 									<tbody>
-										".$displayContact[$line->id_university]."
+										" . $displayContact [$line->id_university] . "
 									</tbody>
 								</table>
 							</td>
 							<td><button>Modify</button></td>
 							<td><button>Delete</button></td>
-						</tr>
 					</tr>";
-					
-				$i++;
+				
+				$i ++;
 			}
 		}
 		return $result;
 	}
-    
-    public function get()
-    {
-		$ci = new CI_CONTROLLER();
-		$id = $ci->uri->segment(4);
+	public function get() {
+		$ci = new CI_CONTROLLER ();
+		$id = $ci->uri->segment ( 4 );
 		
-		$university = new University($id);
+		$university = new University ( $id );
 		
-		exit (json_encode(
-							array(
-									"id" => $university->getId(), 
-									"name" => $university->getName(), 
-									"address" => $university->getAdress(),
-									"country" => $university->getCountry(), 
-									"subscription" => $university->getSubscription(), 
-									"checking state" => $university->getCheckingState(),  
-									"comment" => $university->getComment()
-							)
-		));
-	 }
-
+		exit ( json_encode ( array (
+				"id" => $university->getId (),
+				"name" => $university->getName (),
+				"address" => $university->getAdress (),
+				"country" => $university->getCountry (),
+				"subscription" => $university->getSubscription (),
+				"checking state" => $university->getCheckingState (),
+				"comment" => $university->getComment () 
+		) ) );
+	}
 	public function getId() {
 		return $this->id;
 	}
-
 	public function getName() {
 		return $this->name;
 	}
-
 	public function getAdress() {
 		return $this->address;
 	}
-
 	public function getCountry() {
 		return $this->country;
 	}
-
 	public function getSubscription() {
 		return $this->subscription;
 	}
-
 	public function getCheckingState() {
 		return $this->checking_state;
 	}
-
 	public function getComment() {
-	return $this->comment;
+		return $this->comment;
 	}
-
-	public function addCommentOnUniversity(){
-		$ci = new CI_CONTROLLER();
-		$this->load->model('university/university_md');
-		$this->load->database();
-
-		$id = $ci->uri->segment(4);
-		$comment = $ci->uri->segment(5);
-	
-		$result = $this->university_md->addCommentOnUniversity($id, $comment);
+	public function addCommentOnUniversity() {
+		$ci = new CI_CONTROLLER ();
+		$this->load->model ( 'university/university_md' );
+		$this->load->database ();
+		
+		$id = $ci->uri->segment ( 4 );
+		$comment = $ci->uri->segment ( 5 );
+		
+		$result = $this->university_md->addCommentOnUniversity ( $id, $comment );
 	}
-
-	public function initialiseValue(){
-		$result = $this->university_md->get($this->id);
-
-		if($result->num_rows()){
-			$data = $result->row();
+	public function initialiseValue() {
+		$result = $this->university_md->get ( $this->id );
+		
+		if ($result->num_rows ()) {
+			$data = $result->row ();
 			$this->name = $data->name;
 			$this->address = $data->address;
 			$this->country = $data->country;
@@ -282,53 +259,46 @@ echo '$displayInterns[6] = '.$displayInterns["6"].'<br />';
 			$this->comment = $data->comment;
 		}
 	}
-	
-	public function verificationAddUniversity()
-	{
-		//  loading of the library
-		$this->load->library('form_validation');
-	 	$this->load->model('university/university_md');
-		$this->load->database(); 
+	public function verificationAddUniversity() {
+		// loading of the library
+		$this->load->library ( 'form_validation' );
+		$this->load->model ( 'university/university_md' );
+		$this->load->database ();
 		
-		$this->form_validation->set_rules('UniversityName', '"University Name"', 'trim|required|alpha_dash|encode_php_tags|xss_clean');
-		$this->form_validation->set_rules('Adress', '"Adress"', 'trim|required|alpha_dash|encode_php_tags|xss_clean');
-		$this->form_validation->set_rules('inputCountry','"Country"', 'trim|required|alpha_dash|encode_php_tags|xss_clean');
-		$this->form_validation->set_rules('inputIntern','"Intern"', 'trim|required|alpha_dash|encode_php_tags|xss_clean');
-	 
-		if($this->form_validation->run())
-		{
-			//  If the form is valid
-			$name = $this->input->post('UniversityName');
-			$address = $this->input->post('Adress');
-			$country = $this->input->post('inputCountry');
+		$this->form_validation->set_rules ( 'UniversityName', '"University Name"', 'trim|required|alpha_dash|encode_php_tags|xss_clean' );
+		$this->form_validation->set_rules ( 'Adress', '"Adress"', 'trim|required|alpha_dash|encode_php_tags|xss_clean' );
+		$this->form_validation->set_rules ( 'inputCountry', '"Country"', 'trim|required|alpha_dash|encode_php_tags|xss_clean' );
+		$this->form_validation->set_rules ( 'inputIntern', '"Intern"', 'trim|required|alpha_dash|encode_php_tags|xss_clean' );
+		
+		if ($this->form_validation->run ()) {
+			// If the form is valid
+			$name = $this->input->post ( 'UniversityName' );
+			$address = $this->input->post ( 'Adress' );
+			$country = $this->input->post ( 'inputCountry' );
 			$subscription = 0;
 			$checking_state = 2;
 			
-			$result = $this->university_md->create($name, $address, $country, $subscription, $checking_state);
+			$result = $this->university_md->create ( $name, $address, $country, $subscription, $checking_state );
 			
-			$this->index();
-		}
-		else
-		{
-			//  Le formulaire est invalide ou vide
-			$address = $this->input->post('Adress');
-			$inputInfoContact = $this->input->post('inputInfoContact');
-			$this->formCompletion($address, $inputInfoContact);
+			$this->index ();
+		} else {
+			// Le formulaire est invalide ou vide
+			$address = $this->input->post ( 'Adress' );
+			$inputInfoContact = $this->input->post ( 'inputInfoContact' );
+			$this->formCompletion ( $address, $inputInfoContact );
 		}
 	}
-	   
-	public function formCompletion($address, $inputInfoContact)
-    {
-		$data = array();
-		$data['allUniv'] = $this->getAllUniversities();
-		$data['allNames'] = Intern::getAllNames();
-		$data['address'] = $address;
-		$data['inputInfoContact'] = $inputInfoContact;
+	public function formCompletion($address, $inputInfoContact) {
+		$data = array ();
+		$data ['allUniv'] = $this->getAllUniversities ();
+		$data ['allNames'] = Intern::getAllNames ();
+		$data ['address'] = $address;
+		$data ['inputInfoContact'] = $inputInfoContact;
 		
-		$this->load->view('university/head');
-		$this->load->view('university/topmenu');
-		$this->load->view('university/leftmenu');
-		$this->load->view('university/body', $data);
-		$this->load->view('university/footer');
-    }
+		$this->load->view ( 'university/head' );
+		$this->load->view ( 'university/topmenu' );
+		$this->load->view ( 'university/leftmenu' );
+		$this->load->view ( 'university/body', $data );
+		$this->load->view ( 'university/footer' );
+	}
 }
