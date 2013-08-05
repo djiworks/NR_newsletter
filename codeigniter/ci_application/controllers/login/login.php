937dbf4c9017fff788199b1f62082c7cac0ce582
hartorn <bazirehoussin@gmail.com>
2013-08-05 12:51:14 +0200

    ajout de la fonctionnalité sign up

--- codeigniter/ci_application/controllers/login/login.php
+++ codeigniter/ci_application/controllers/login/login.php
@@ -7,14 +7,21 @@ class Login extends CI_Controller {
 		$this->load->helper('login');
 	 }
 	 
-	 function index()
+	 function index($is_success = NULL)
 	 {
 		if($this->session->userdata('logged_in'))
 		{
 			redirect('university/university', 'refresh');
 		}
+		$data = array();
+		
+		if(isset($is_success))
+		{
+			$data['is_success'] = $is_success;
+		}
+		
 	 	$this->load->view('login/head');
-	   	$this->load->view('login/login_view');
+	   	$this->load->view('login/login_view', $data);
 	   	$this->load->view('login/footer');
 	 }
 	 
@@ -25,6 +32,42 @@ class Login extends CI_Controller {
 	 	session_destroy();
 	 	redirect('login/login', 'refresh');
 	 }
+	 
+	 public function verificationAddUser() {
+		//~ isLoggedInRedirect($this);
+		
+		// loading of the library
+		$this->load->library ( 'form_validation' );
+		$this->load->model ( 'user/user_md' );
+		
+		$this->form_validation->set_rules ( 'Login', '"Login"', 'trim|required|encode_php_tags|xss_clean' );
+		$this->form_validation->set_rules ( 'Password', '"Password"', 'trim|required|encode_php_tags|xss_clean' );
+		$this->form_validation->set_rules ( 'ConfirmPassword', '"ConfirmPassword"', 'trim|required|encode_php_tags|xss_clean' );
+		
+		if ($this->form_validation->run ()) {
+			// If the form is valid
+			$login = $this->input->post ( 'Login' );
+			$password = crypt($this->input->post ( 'Password' ));
+			$confirm_password = crypt($this->input->post ( 'ConfirmPassword' ),$password);
+			$role = 5;
+			
+			if($password == $confirm_password)
+			{
+				$result = $this->user_md->create ( $login, $password, $role);
+				//~ echo '0';
+				$this->index (0);
+			}
+			else
+			{
+				//~ echo '1';
+				$this->index (1);
+			}
+		} else {
+			// If the form is not valid or empty
+			$this->index (1);
+			//~ echo '2';
+		}
+	}
 	 
 }	 
 ?>
--- codeigniter/ci_application/views/login/login_view.php
+++ codeigniter/ci_application/views/login/login_view.php
@@ -10,13 +10,79 @@
 			<h2 class="form-signin-heading">Please sign in</h2>
 			<input type="text" class="input-block-level" placeholder="Username" id="username" name="username"> 
 			<input type="password" class="input-block-level" placeholder="Password" id="passowrd" name="password"> 
-			<a href="index.php">
-				<button class="btn btn-large btn-primary" type="submit">Log in</button>
-			</a> 
-			<a href="registration.php">
-				<button class="btn btn-large btn-primary pull-right" type="button" data-toggle='modal' data-target='#signUp'>Sign up</button>
-			</a>
+			<button class="btn btn-large btn-primary" type="submit">Log in</button>
+			<button class="btn btn-large btn-primary pull-right" type="button" data-toggle='modal' data-target='#addUser'>Sign up</button>
 		</form>
 
 
+	</div>
+
+<div id="addUser" class="modal hide fade" tabindex="-1"
+		role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal"
+				aria-hidden="true">x</button>
+			<h3 id="addUserLabel">Add an User</h3>
+		</div>
+		<div class="modal-body">
+			<form method="post" action="/index.php/login/login/verificationAddUser" class="form-horizontal">
+				<div class="control-group">
+					<label class="control-label" for="Login">Login</label>
+					<div class="controls">
+						<input type="text" id="Login" name="Login" placeholder="Login" value="<?php echo set_value('Login'); ?>" />
+						<?php echo form_error('Login'); ?>
+					</div>
+				</div>
+				
+				<div class="control-group">
+					<label class="control-label" for="Password">Password</label>
+					<div class="controls">
+						<input type="password" id="Password" name="Password" placeholder="Password" />
+					</div>
+				</div>
+				
+				<div class="control-group">
+					<label class="control-label" for="ConfirmPassword">Confirm Password</label>
+					<div class="controls">
+						<input type="password" id="ConfirmPassword" name="ConfirmPassword" placeholder="Confirm Password"/>
+					</div>
+				</div>
+
+				<div class="control-group">
+					<div class="controls">
+						<button type="submit" class="btn">Submit</button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="modal-footer">
+			<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+		</div>
+	</div>
+	
+	<div id="success" class="modal hide fade">
+	<div class="modal-header">
+	<button type="button" class="close" data-dismiss="modal" aria-hidden="true" onclick="window.location.href = '/index.php/admin/admin';">&times;</button>
+	<h4>Success</h4>
+	</div>
+	<div class="modal-body">
+	<p>Account created with success. </br>Please wait for an administrator to validate your account.</p>
+	</div>
+	<div class="modal-footer">
+	<button class="btn" type="button" data-dismiss="modal" onclick="window.location.href = '/index.php/admin/admin';">Close</button>
+	</div>
+	</div>
+	
+	
+	<div id="failure" class="modal hide fade">
+	<div class="modal-header">
+	<button type="button" class="close" data-dismiss="modal" data-toggle='modal' data-target='#addUser' aria-hidden="true">&times;</button>
+	<h4>Failure</h4>
+	</div>
+	<div class="modal-body">
+	<p>Failed to add user. Please fill in all the fields.</p>
+	</div>
+	<div class="modal-footer">
+	<button class="btn" type="button" data-dismiss="modal" data-toggle='modal' data-target='#addUser'>Close</button>	
+	</div>
 	</div>
